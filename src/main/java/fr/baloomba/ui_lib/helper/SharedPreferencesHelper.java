eae5930e18aa5fce9c778d039641b628de981439
Clement BOULY <bouly.clement@gmail.com>
2014-10-31 18:18:21 +0100

    Update

--- src/main/java/fr/baloomba/ui_lib/helper/SharedPreferencesHelper.java
+++ src/main/java/fr/baloomba/ui_lib/helper/SharedPreferencesHelper.java
@@ -7,25 +7,23 @@ public class SharedPreferencesHelper {
 
     // <editor-fold desc="STATIC VARIABLES">
 
-    private static final String PREFERENCES_NAME = "Yummypets";
-
     // </editor-fold>
 
     // <editor-fold desc="BOOLEAN METHODS">
 
-    public static Boolean getBoolean(Context context, String key, Boolean defaultValue) {
+    public static Boolean getBoolean(Context context, String name, String key, Boolean defaultValue) {
         if (context.getApplicationContext() == null)
             return defaultValue;
         SharedPreferences settings = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                .getSharedPreferences(name, Context.MODE_PRIVATE);
         return settings.getBoolean(key, defaultValue);
     }
 
-    public static void putBoolean(Context context, String key, Boolean value) {
+    public static void putBoolean(Context context, String name, String key, Boolean value) {
         if (context.getApplicationContext() == null)
             return;
         SharedPreferences.Editor editor = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                .getSharedPreferences(name, Context.MODE_PRIVATE).edit();
         editor.putBoolean(key, value);
 //        editor.commit();
         editor.apply();
@@ -35,19 +33,19 @@ public class SharedPreferencesHelper {
 
     // <editor-fold desc="FLOAT METHODS">
 
-    public static Float getFloat(Context context, String key, Float defaultValue) {
+    public static Float getFloat(Context context, String name, String key, Float defaultValue) {
         if (context.getApplicationContext() == null)
             return defaultValue;
         SharedPreferences settings = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                .getSharedPreferences(name, Context.MODE_PRIVATE);
         return settings.getFloat(key, defaultValue);
     }
 
-    public static void putFloat(Context context, String key, Float value) {
+    public static void putFloat(Context context, String name, String key, Float value) {
         if (context.getApplicationContext() == null)
             return;
         SharedPreferences.Editor editor = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                .getSharedPreferences(name, Context.MODE_PRIVATE).edit();
         editor.putFloat(key, value);
 //        editor.commit();
         editor.apply();
@@ -57,19 +55,19 @@ public class SharedPreferencesHelper {
 
     // <editor-fold desc="INT METHODS">
 
-    public static int getInt(Context context, String key, int defaultValue) {
+    public static int getInt(Context context, String name, String key, int defaultValue) {
         if (context.getApplicationContext() == null)
             return defaultValue;
         SharedPreferences settings = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                .getSharedPreferences(name, Context.MODE_PRIVATE);
         return settings.getInt(key, defaultValue);
     }
 
-    public static void putInt(Context context, String key, int value) {
+    public static void putInt(Context context, String name, String key, int value) {
         if (context.getApplicationContext() == null)
             return;
         SharedPreferences.Editor editor = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                .getSharedPreferences(name, Context.MODE_PRIVATE).edit();
         editor.putInt(key, value);
 //        editor.commit();
         editor.apply();
@@ -79,19 +77,19 @@ public class SharedPreferencesHelper {
 
     // <editor-fold desc="LONG METHODS">
 
-    public static Long getLong(Context context, String key, Long defaultValue) {
+    public static Long getLong(Context context, String name, String key, Long defaultValue) {
         if (context.getApplicationContext() == null)
             return defaultValue;
         SharedPreferences settings = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                .getSharedPreferences(name, Context.MODE_PRIVATE);
         return settings.getLong(key, defaultValue);
     }
 
-    public static void putLong(Context context, String key, Long value) {
+    public static void putLong(Context context, String name, String key, Long value) {
         if (context.getApplicationContext() == null)
             return;
         SharedPreferences.Editor editor = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                .getSharedPreferences(name, Context.MODE_PRIVATE).edit();
         editor.putLong(key, value);
 //        editor.commit();
         editor.apply();
@@ -101,19 +99,19 @@ public class SharedPreferencesHelper {
 
     // <editor-fold desc="STRING METHODS">
 
-    public static String getString(Context context, String key, String defaultValue) {
+    public static String getString(Context context, String name, String key, String defaultValue) {
         if (context.getApplicationContext() == null)
             return defaultValue;
         SharedPreferences settings = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+                .getSharedPreferences(name, Context.MODE_PRIVATE);
         return settings.getString(key, defaultValue);
     }
 
-    public static void putString(Context context, String key, String value) {
+    public static void putString(Context context, String name, String key, String value) {
         if (context.getApplicationContext() == null)
             return;
         SharedPreferences.Editor editor = context.getApplicationContext()
-                .getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                .getSharedPreferences(name, Context.MODE_PRIVATE).edit();
         editor.putString(key, value);
 //        editor.commit();
         editor.apply();
